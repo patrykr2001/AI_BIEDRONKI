02babf34fb7d8bce85356c87b1703bdc52572270
Patryk Rakowski <95425713+patrykr2001@users.noreply.github.com>
2025-01-08 16:21:02 +0100

    Add Zut API commands to Enum classes.

---
+++ Scrapper/src/Enum/ZutDataKinds.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Enum;
+
+enum ZutDataKinds: string
+{
+    case Teachers = 'teacher';
+    case Rooms = 'room';
+    case Subjects = 'subject';
+    case Groups = 'group';
+}
