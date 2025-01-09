b17a6f446f95f8c8e87bf6bce087bdfa1b2af48e
Patryk Rakowski <95425713+patrykr2001@users.noreply.github.com>
2025-01-09 17:33:02 +0100

    Update server version to '15.8' in doctrine.yaml and handle null URL in command.

--- Scrapper/src/Command/ScrapZutApiCommand.php
+++ Scrapper/src/Command/ScrapZutApiCommand.php
@@ -4,30 +4,30 @@ declare(strict_types=1);
 
 namespace App\Command;
 
-use App\Enum\ZutDataKinds;
-use App\Service\ZutUrlBuilder;
+use App\Config\ConfigReader;
 use App\Service\ZutDataUpdater;
+use App\Service\ZutUrlBuilder;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Input\InputArgument;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\HttpClient\HttpClient;
-use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 #[AsCommand(name: 'app:scrap_zut_api', description: 'Run scrapper for ZUT API')]
 class ScrapZutApiCommand extends Command
 {
     protected function configure()
     {
-        $this
-            ->addArgument('url', InputArgument::OPTIONAL, 'The API URL to fetch data from',
-            'https://plan.zut.edu.pl/');
+        $this->addArgument('url', InputArgument::OPTIONAL, 'The API URL to fetch data from',
+            null);
     }
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $url = $input->getArgument('url');
+        if (!$url) {
+            $url = (new ConfigReader())->getApiBaseUrl();
+        }
 
         $client = HttpClient::create();
         $urlBuilder = new ZutUrlBuilder($url);
