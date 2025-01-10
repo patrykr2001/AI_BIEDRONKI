ac309133f1711c1583c65f02d6ba02819d470e74
Patryk Rakowski <95425713+patrykr2001@users.noreply.github.com>
2025-01-11 00:58:32 +0100

    Refactor findLessonByTeacherSubjectGroupStartEnd method for error handling.

--- Scrapper/src/Repository/LessonRepository.php
+++ Scrapper/src/Repository/LessonRepository.php
@@ -71,19 +71,25 @@ class LessonRepository extends ServiceEntityRepository
     public function findLessonByTeacherSubjectGroupStartEnd(Teacher  $teacher, Subject $subject, ?Group $group,
                                                             DateTime $start, DateTime $end): ?Lesson
     {
-        return $this->createQueryBuilder('l')
-            ->andWhere('l.workerId = :teacher')
-            ->andWhere('l.subjectId = :subject')
-            ->andWhere('l.groupId = :group')
-            ->andWhere('l.startDate >= :start')
-            ->andWhere('l.endDate <= :end')
-            ->setParameter('teacher', $teacher)
-            ->setParameter('subject', $subject)
-            ->setParameter('group', $group)
-            ->setParameter('start', $start)
-            ->setParameter('end', $end)
-            ->getQuery()
-            ->getOneOrNullResult();
+        try {
+            return $this->createQueryBuilder('l')
+                ->andWhere('l.workerId = :teacher')
+                ->andWhere('l.subjectId = :subject')
+                ->andWhere('l.groupId = :group')
+                ->andWhere('l.startDate >= :start')
+                ->andWhere('l.endDate <= :end')
+                ->setParameter('teacher', $teacher)
+                ->setParameter('subject', $subject)
+                ->setParameter('group', $group)
+                ->setParameter('start', $start)
+                ->setParameter('end', $end)
+                ->getQuery()
+                ->getOneOrNullResult();
+        } catch (\Exception $e) {
+            echo $e->getMessage();
+            return null;
+        }
+
     }
 
     /**
