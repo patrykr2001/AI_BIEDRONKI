808b059dfcf3f92d25663564225231e5dd2a7c67
Patryk Rakowski <95425713+patrykr2001@users.noreply.github.com>
2025-01-10 11:56:53 +0100

    Update start date in date_range to '2024-10-01'.

--- Scrapper/config.php
+++ Scrapper/config.php
@@ -5,7 +5,7 @@
 return [
     'api_base_url' => 'https://plan.zut.edu.pl/',
     'date_range' => [
-        'start' => '2025-01-01',
+        'start' => '2024-10-01',
         'end' => '2025-01-31',
     ],
 ];
